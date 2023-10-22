bfc78632e396166ee571651bfd8ef5f7093a0214
srknssmn <srknssmn@gmail.com>
2023-10-23 00:35:36 +0300

    update

--- public/js/donateSelect.js
+++ public/js/donateSelect.js
@@ -1,6 +1,7 @@
 const donateButton = document.querySelector('#donateButton')
 let donateCost = document.querySelector('#donateCost')
 let donateSelect = document.querySelector('#donateSelect')
+let selectElement = document.querySelector('#selectElement')
 const buttonSelects = document.querySelectorAll('.buttonSelect');
 
 buttonSelects.forEach(button => {
@@ -9,34 +10,42 @@ buttonSelects.forEach(button => {
       button.classList.add('active');
       if(button.classList.contains('active') && (button.getAttribute('id') == "thanks")) {
           donateCost.value = "0.0001 ETH"
+          selectElement.innerHTML = "WATER"
           donateSelect.value = 1
           donateButton.disabled = false;
       } else if (button.classList.contains('active') && (button.getAttribute('id') == "tea")) {
         donateCost.value = "0.001 ETH"
+        selectElement.innerHTML = "TEA"
         donateSelect.value = 2
         donateButton.disabled = false;
       } else if (button.classList.contains('active') && (button.getAttribute('id') == "coffee")) {
         donateCost.value = "0.002 ETH"
+        selectElement.innerHTML = "COFFEE"
         donateSelect.value = 3
         donateButton.disabled = false;
       } else if (button.classList.contains('active') && (button.getAttribute('id') == "beer")) {
         donateCost.value = "0.003 ETH"
+        selectElement.innerHTML = "BEER"
         donateSelect.value = 4
         donateButton.disabled = false;
       } else if (button.classList.contains('active') && (button.getAttribute('id') == "cocktail")) {
         donateCost.value = "0.005 ETH"
+        selectElement.innerHTML = "COCKTAIL"
         donateSelect.value = 5
         donateButton.disabled = false;
       } else if (button.classList.contains('active') && (button.getAttribute('id') == "whiskey")) {
         donateCost.value = "0.010 ETH"
+        selectElement.innerHTML = "WHISKEY"
         donateSelect.value = 6
         donateButton.disabled = false;
       } else if (button.classList.contains('active') && (button.getAttribute('id') == "lionmilk")) {
         donateCost.value = "0.10 ETH"
+        selectElement.innerHTML = "LION'S MILK"
         donateSelect.value = 7
         donateButton.disabled = false;
       } else if (button.classList.contains('active') && (button.getAttribute('id') == "kimiz")) {
         donateCost.value = "1.0 ETH"
+        selectElement.innerHTML = "DRAGON BLOOD"
         donateSelect.value = 8
         donateButton.disabled = false;
       }
