f83411e60253da65b5b2cc0ed4b05d33f6a42422
srknssmn <srknssmn@gmail.com>
2023-10-23 02:25:54 +0300

    update

--- public/js/detectWallet.js
+++ public/js/detectWallet.js
@@ -4,6 +4,8 @@ window.onload = (event) => {
 
 let connectWalletButton = document.querySelector('#connectWallet')
 let donateSection = document.querySelector('#donateSection')
+let donorsSection = document.querySelector('#donorsSection')
+let connectSection = document.querySelector('#connectSection')
 
 async function isConnected() {
     const accounts = await ethereum.request({method: 'eth_accounts'});       
@@ -15,8 +17,12 @@ async function isConnected() {
         let last = await userWallet.slice(-5)
         connectWalletButton.innerHTML = await first + "..." + last
         donateSection.hidden = await false;
+        donorsSection.hidden = await false;
+        connectSection.hidden = await true;
     } else {
         console.log("Metamask is not connected");
         donateSection.hidden = await true;
+        donorsSection.hidden = await true;
+        connectSection.hidden = await false;
     }
 }
